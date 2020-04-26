aace30144723b3a7461fe33d98b1fa3bcf83d686
Amirreza <amirgarshasbi88@gmail.com>
2020-04-26 13:35:57 +0430

    Secure session true

--- config/session.js
+++ config/session.js
@@ -5,6 +5,6 @@ module.exports ={
     secret :process.env.SECRET_SESSION,
     resave : true,
     saveUninitialized : true,
-    cookie:{expires:new Date(Date.now()+1000*60*60*2),SameSite:true,secure:false},
+    cookie:{expires:new Date(Date.now()+1000*60*60*6),SameSite:true,Secure:true},
     store : new MongoStore({ mongooseConnection : mongoose.connection })
 }
