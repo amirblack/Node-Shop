bdafdec3b842c2b329ac81a5593dc6628e3f96ae
Amirreza <amirgarshasbi88@gmail.com>
2020-04-27 12:23:42 +0430

    add 6hours to session

--- config/session.js
+++ config/session.js
@@ -5,6 +5,6 @@ module.exports ={
     secret :process.env.SECRET_SESSION,
     resave : true,
     saveUninitialized : true,
-    cookie:{expires:new Date(Date.now()+1000*60*60*6),SameSite:true,Secure:true},
+    cookie:{expires:new Date(Date.now()+1000*60*60*12),SameSite:true,Secure:true},
     store : new MongoStore({ mongooseConnection : mongoose.connection })
 }
